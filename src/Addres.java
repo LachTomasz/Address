516deebfb81dcb2c85ea5b4bc274c8484a16aed8
LachTomasz <lach.tomasz@gmail.com>
2022-02-06 17:35:00 +0100

    Initial commit

---
+++ src/Addres.java
@@ -0,0 +1,8 @@
+public class Addres {
+    public static void main(String[] args) {
+        System.out.println("My name and surname");
+        System.out.println("My street and street number");
+        System.out.println("My ZIP and city");
+        System.out.println("My county");
+    }
+}
